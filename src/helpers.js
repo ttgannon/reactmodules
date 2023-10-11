c68dd554980f78301b4ad279c838171336b9bb04
ttgannon <ga1179@nyu.edu>
2023-10-11 16:57:10 -0300

    modules exercise

---
+++ src/helpers.js
@@ -0,0 +1,15 @@
+function choice(items) {
+    const randIdx = Math.floor(Math.random() * items.length);
+    return items[randIdx];
+}
+
+function remove(items, item) {
+    const index = items.indexOf(item);
+    if (index !== -1) {
+        return items.splice(index, 1);
+    }
+    return undefined;
+}
+
+
+export {choice, remove};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {choice, remove} from './helpers';
+import fruits from './foods'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let fruit = choice(fruits);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function fruitDraw() {
+    console.log(`I'd like one ${fruit}, please.`);
+    console.log(`Here you go: ${fruit}`)
+    console.log(`Delicious! May I have another?`)
+    let remaining = remove(fruits, fruit);
+    console.log(`i'm sorry, no more! We have ${remaining.length} left`);
+}
